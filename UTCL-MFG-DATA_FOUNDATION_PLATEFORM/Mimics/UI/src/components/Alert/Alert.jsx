7847ffb5a3ee2cdb4f10855ee2c20d50f80d16d7
sejal-1805 <sejal.kotkar@tridiagonal.ai>
2026-04-20 17:37:44 +0530

    trends, chart visualization

--- UTCL-MFG-DATA_FOUNDATION_PLATEFORM/Mimics/UI/src/components/Alert/Alert.jsx
+++ UTCL-MFG-DATA_FOUNDATION_PLATEFORM/Mimics/UI/src/components/Alert/Alert.jsx
@@ -1,7 +1,16 @@
 export function Alert({ errMsg }) {
   return (
-    <div className="alert alert-danger mt-1" role="alert">
-      {errMsg}
-    </div>
+    // <div className="alert alert-danger mt-1" role="alert">
+    //   {errMsg}
+    // </div>
+    <div>
+    <p className="mt-5 flex flex-rowitems-center gap-3 justify-center text-center rounded-xl transition-all duration-300 animate-fadeIn">
+      <div class="text-xl mb-4 zoom-animation">🔍</div>
+      {/* <!-- Title --> */}
+      <h2 class="text-lg font-semibold text-[var(--leftdrawer-text)] mb-2">
+              {errMsg}
+      </h2>
+    </p>
+  </div>
   );
 }
